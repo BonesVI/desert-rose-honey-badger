1914d512277c202e8a48513ab7daf4a1b8de0a41
Rohan Pavone <rohan.pavone@gmail.com>
2012-08-22 21:41:57 -0400

    Generated Rails 3 app, added git repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '69881fcb34fa80e824e24d94c4a9ae3bd0997d52a624f8a0ba5d68ed8d4436ffe81ad69524a421e0cf3477c97f2efe3fe731d19eed13e3c4ddfc728c6ac7797a'
